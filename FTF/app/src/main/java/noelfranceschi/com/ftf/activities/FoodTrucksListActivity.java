43fa933b324fc2e1f42ea33cd43b40c33832582d
Rotor7 <Rotor7@users.noreply.github.com>
2017-10-13 11:23:45 -0400

    Project created
    - Set up the data model

---
+++ FTF/app/src/main/java/noelfranceschi/com/ftf/activities/FoodTrucksListActivity.java
@@ -0,0 +1,76 @@
+package noelfranceschi.com.ftf.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import noelfranceschi.com.ftf.R;
+import noelfranceschi.com.ftf.model.FoodTruck;
+
+
+public class FoodTrucksListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_trucks_list);
+
+        String url = "https://finalwebtest.com/api/v1/foodtruck";
+        final ArrayList<FoodTruck> foodTruckList = new ArrayList<>();
+
+        final JsonArrayRequest getTrucks = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                System.out.println(response.toString());
+
+                try {
+                    JSONArray foodTrucks = response;
+
+                    for (int x = 0; x < foodTrucks.length(); x++) {
+                        JSONObject foodTruck = foodTrucks.getJSONObject(x);
+                        String name = foodTruck.getString("name");
+                        String id = foodTruck.getString("_id");
+                        String foodType = foodTruck.getString("foodtype");
+                        Double avgCost = foodTruck.getDouble("avgCost");
+
+                        JSONObject geometry = foodTruck.getJSONObject("geometry");
+                        JSONObject coordinates = geometry.getJSONObject("coordinates");
+
+                        Double latitude = coordinates.getDouble("lat");
+                        Double longitude = coordinates.getDouble("long");
+
+                        FoodTruck newFoodTruck = new FoodTruck(id, name, foodType, avgCost, latitude, longitude);
+                        foodTruckList.add(newFoodTruck);
+                    }
+
+                } catch (JSONException e) {
+                    Log.v("JSON", "EXC" + e.getLocalizedMessage());
+                }
+
+//                System.out.print("THIS IS THE FOOD TRUCK NAME: " + foodTruckList.get(0).getName());
+
+
+            }
+        }, new Response.ErrorListener(){
+           @Override
+            public void onErrorResponse(VolleyError error) {
+               Log.v("API", "Err" + error.getLocalizedMessage());
+           }
+        });
+
+        Volley.newRequestQueue(this).add(getTrucks);
+    }
+}
---
+++ FTF/app/src/main/java/noelfranceschi/com/ftf/model/FoodTruck.java
@@ -0,0 +1,48 @@
+package noelfranceschi.com.ftf.model;
+
+/**
+ * Created by noelfranceschi on 10/10/17.
+ */
+
+public class FoodTruck {
+
+    private String id = "";
+    private String name = "";
+    private String foodType = "";
+    private Double avgCost = 0.0;
+    private Double latitude = 0.0;
+    private Double longitude = 0.0;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFoodType() {
+        return foodType;
+    }
+
+    public Double getAvgCost() {
+        return avgCost;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public FoodTruck(String id, String name, String foodType, Double avgCost, Double latitude, Double longitude) {
+        this.id = id;
+        this.name = name;
+        this.foodType = foodType;
+        this.avgCost = avgCost;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
